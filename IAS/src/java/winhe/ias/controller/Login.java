a48c5e5d04d55bb84102e7f4cc3018894aeefc4e
Themiya77 <themiyaranasinghe1@gmail.com>
2017-11-28 11:29:45 +0530

    modified the Login

--- IAS/src/java/winhe/ias/controller/Login.java
+++ IAS/src/java/winhe/ias/controller/Login.java
@@ -36,7 +36,7 @@ public class Login extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet Login</title>");            
+            out.println("<title>Servlet Login</title>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet Login at " + request.getContextPath() + "</h1>");
@@ -55,23 +55,6 @@ public class Login extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-//        processRequest(request, response);
-        HttpSession session = request.getSession();
-        session.invalidate();
-        response.sendRedirect("/IAS");
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 //        processRequest(request, response);
@@ -79,7 +62,7 @@ public class Login extends HttpServlet {
         String password = request.getParameter("password");
 
         if (!email.isEmpty() && !password.isEmpty()) {
-            
+
             if (!email.equals("super_admin")) {
                 HttpSession session = request.getSession();
 
